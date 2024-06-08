1c2d8591530e96ca84b821efc41f7e056f817154
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-06-08 16:21:39 +0100

    Added a docker-compose.yaml file

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -10,14 +10,49 @@ default_args = {
 
 
 
+
+def get_data():
+    import json
+    import requests as r
+
+    res = r.get('https://randomuser.me/api/').json()
+    res = res['results'][0]
+    # print(json.dumps(res, indent=3))
+
+    return res
+
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['first_name'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']} \
+        {location['city']} {location['state']} {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
 def stream_data():
     import json
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
+
 
 
 with DAG(
     'user_automation',
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule='@daily',
     catchup=False
          ) as dag:
     
@@ -25,3 +60,6 @@ with DAG(
         task_id='streaming_data_from_api',
         python_callable=stream_data
     )
+
+
+stream_data()
